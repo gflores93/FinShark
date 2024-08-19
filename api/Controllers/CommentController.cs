2566e6b0ce3ac3675e8cae85dc788c0a0b93e60a
Guillermo Flores <gflores.93@hotmail.com>
2024-08-19 17:16:50 -0600

    Add: 1-to-Many POST

--- api/Controllers/CommentController.cs
+++ api/Controllers/CommentController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using api.Dtos.Comment;
 using api.Interfaces;
 using api.Mappers;
 using Microsoft.AspNetCore.Mvc;
@@ -15,10 +16,13 @@ namespace api.Controllers
     public class CommentController : ControllerBase
     {
         private readonly ICommentRepository _commentRepo;
+        private readonly IStockRepository   _stockRepo;
 
-        public CommentController(ICommentRepository commentRepo)
+
+        public CommentController(ICommentRepository commentRepo, IStockRepository stockRepo)
         {
             _commentRepo = commentRepo;
+            _stockRepo = stockRepo;
         }
 
         [HttpGet]
@@ -39,5 +43,17 @@ namespace api.Controllers
 
             return Ok(comment.ToCommentDto());
         }
+
+        [HttpPost("{stockId}")]
+        public async Task<IActionResult> Create([FromRoute] int stockId, CreateCommentDto commentDto) {
+            if(!await _stockRepo.StockExists(stockId)) {
+                return BadRequest("Stock does not exist");
+            }
+            
+            var commentModel = commentDto.ToCommentFromCreate(stockId);
+            await _commentRepo.CreateAsync(commentModel);
+            return CreatedAtAction(nameof(GetById), new {id = commentModel.Id}, commentModel.ToCommentDto());      
+        }
+
     }
 }
