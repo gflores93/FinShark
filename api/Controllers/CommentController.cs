13d7e84f8a1df56ad69993f8ebd7d50946691c8d
Guillermo Flores <gflores.93@hotmail.com>
2024-08-19 17:47:36 -0600

    Add: Comment PUT (update)

--- api/Controllers/CommentController.cs
+++ api/Controllers/CommentController.cs
@@ -55,5 +55,16 @@ namespace api.Controllers
             return CreatedAtAction(nameof(GetById), new {id = commentModel.Id}, commentModel.ToCommentDto());      
         }
 
+        [HttpPut]
+        [Route("{id}")]
+        public async Task<IActionResult> Update([FromRoute] int id, [FromBody] UpdateCommentRequestDto updateDto) {
+            var comment = await _commentRepo.UpdateAsync(id, updateDto.ToCommentFromUpdate());
+            if (comment == null) {
+                return NotFound("Comment not found");
+            }
+
+            return Ok(comment.ToCommentDto());
+        }
+
     }
 }
--- api/Mappers/CommentMapper.cs
+++ api/Mappers/CommentMapper.cs
@@ -9,8 +9,10 @@ namespace api.Mappers
 {
     public static class CommentMapper
     {
-        public static CommentDto ToCommentDto(this Comment commentModel) {
-            return new CommentDto {
+        public static CommentDto ToCommentDto(this Comment commentModel)
+        {
+            return new CommentDto
+            {
                 Id = commentModel.Id,
                 Title = commentModel.Title,
                 Content = commentModel.Content,
@@ -19,11 +21,22 @@ namespace api.Mappers
             };
         }
 
-        public static Comment ToCommentFromCreate(this CreateCommentDto commentDto, int stockId) {
-            return new Comment {
+        public static Comment ToCommentFromCreate(this CreateCommentDto commentDto, int stockId)
+        {
+            return new Comment
+            {
                 Title = commentDto.Title,
                 Content = commentDto.Content,
                 StockId = stockId
+            };
+        }
+
+        public static Comment ToCommentFromUpdate(this UpdateCommentRequestDto commentDto)
+        {
+            return new Comment
+            {
+                Title = commentDto.Title,
+                Content = commentDto.Content
             };
         }
     }
