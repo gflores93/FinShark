5c1028a5d04f672e8f4283603da58fc93425c213
Guillermo Flores <gflores.93@hotmail.com>
2024-08-19 19:50:15 -0600

    Add: Sorting by Symbol included

--- api/Repository/StockRepository.cs
+++ api/Repository/StockRepository.cs
@@ -40,6 +40,8 @@ namespace api.Repository
         {
 
             var stocks = _context.Stocks.Include(c => c.Comments).AsQueryable();
+
+            // Filtering
             if (!string.IsNullOrWhiteSpace(query.CompanyName))
             {
                 stocks = stocks.Where(s => s.CompanyName.Contains(query.CompanyName));
@@ -50,6 +52,13 @@ namespace api.Repository
                 stocks = stocks.Where(s => s.Symbol.Contains(query.Symbol));
             }
 
+            // Sorting
+            if(!string.IsNullOrWhiteSpace(query.SortBy)) {
+                if(query.SortBy.Equals("Symbol", StringComparison.OrdinalIgnoreCase)) {
+                    stocks = query.IsDescending ? stocks.OrderByDescending(s => s.Symbol) : stocks.OrderBy(s => s.Symbol);
+                }
+                //If another sorting property is required we can use the same logic
+            }
             return await stocks.ToListAsync();
         }
 
